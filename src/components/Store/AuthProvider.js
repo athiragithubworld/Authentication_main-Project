73dfe1467fd986db17d67f423b7e7f8537aef5f1
athiragithubworld <athirahrms@gmail.com>
2023-05-31 22:02:57 +0530

    AuthContext and Provider (Store token)

---
+++ src/components/Store/AuthProvider.js
@@ -0,0 +1,31 @@
+import AuthContext from "./AuthContext";
+import React, { useState } from "react";
+
+const AuthProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextvalue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextvalue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
